160a27d7b5e35fde8cf724acb7d8e58569da12fd
metromancn <metromancn@gmail.com>
2025-04-22 13:07:36 +0900

    Lines

---
+++ ASP250422/MetroMan/MMWeb/Controllers/LinesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MMWeb.Controllers
+{
+    [RoutePrefix("en/cities/shanghai/lines")]
+    public class LinesController : Controller
+    {
+        // GET /en/cities/shanghai/lines
+        [HttpGet, Route("")]
+        public ActionResult Index() => View();
+    }
+}
