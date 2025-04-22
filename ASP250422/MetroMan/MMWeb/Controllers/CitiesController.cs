424cbd36d183b7192ed930edf15136de8e50ad26
metromancn <metromancn@gmail.com>
2025-04-22 10:19:06 +0900

    RouteConfig & CitiesController

---
+++ ASP250422/MetroMan/MMWeb/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MMWeb.Controllers
+{
+    [RoutePrefix("en/cities")]
+    public class CitiesController : Controller
+    {
+        // GET /en/cities
+        [HttpGet, Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
