203446135ca86818dd0a0cae2eb3d05646423f7e
metromancn <metromancn@gmail.com>
2025-04-22 11:03:19 +0900

    Shanghai

---
+++ ASP250422/MetroMan/MMWeb/Controllers/ShanghaiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MMWeb.Controllers
+{
+    [RoutePrefix("en/cities/shanghai")]
+    public class ShanghaiController : Controller
+    {
+        // GET /en/cities/shanghai
+        [HttpGet, Route("")]
+        public ActionResult Index() => View();
+
+        // GET /en/cities/shanghai/metro-map
+        [HttpGet, Route("metro-map")]
+        public ActionResult MetroMap() => View();
+
+        // GET /en/cities/shanghai/planning-map
+        [HttpGet, Route("planning-map")]
+        public ActionResult PlanningMap() => View();
+    }
+}
