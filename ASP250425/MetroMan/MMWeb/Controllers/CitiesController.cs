210e5404a983dad0ac9d70f43e368a3e7c3bf71a
metromancn <metromancn@gmail.com>
2025-04-28 13:51:29 +0900

    Add Cities

---
+++ ASP250425/MetroMan/MMWeb/Controllers/CitiesController.cs
@@ -0,0 +1,13 @@
+using System.Web.Mvc;
+
+namespace MMWeb.Controllers
+{
+    public class CitiesController : Controller
+    {
+        // GET: /Cities/
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
